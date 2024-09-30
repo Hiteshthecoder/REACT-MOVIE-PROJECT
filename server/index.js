8a364b4aa2cf1864ec87a33912d5faaaecfdfeac
hitesh <hiteshparmar16320@gmail.com>
2024-09-30 13:52:46 +0530

    post method completed

--- server/index.js
+++ server/index.js
@@ -2,19 +2,31 @@
 // express js is an middleware that is used to handle user requests in backend.
 
 const Server = require("express");
+const { json } = require("react-router-dom");
 
 const app = Server();
 
 // get request
 
+// XML
+
+app.use(Server.json());
+
 app.get("/sayHello", function (req, res) {
-    res.send("Hello  from node js backend");
+    res.sendFile("C:/hitesh/WEB_DESIGNING/FULL-STACK/REACT/basics/server/hello.html");
+});
+
+// post method in express js
+app.post("/getData", function (req, res) {
+    let data = req.body;
+    let { name, hobby } = data;
+
+    res.json({ name, hobby })
 })
+
 
 app.listen(5500, function () {
     console.log("server is online now");
 })
-
-
 
 // app.use();
